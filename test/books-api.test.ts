5e7670820d114f893eec52e89daebeccdff5aa91
Ben Ooms <benoomsdh@gmail.com>
2022-07-28 13:50:06 +0200

    Initial commit

---
+++ test/books-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BooksApi from '../lib/books-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/books-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BooksApi.BooksApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
